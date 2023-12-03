f12b5fa60a75bded95a158a61bfdf402d1e68056
noyes5 <noyes5@naver.com>
2023-12-03 17:22:53 +0900

    refactor(Output): 출력 메시지 리팩토링

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -6,6 +6,7 @@ import static subway.util.CompleteMessage.COMPLETE_SECTION_DELETE;
 import static subway.util.CompleteMessage.COMPLETE_SECTION_REGISTRATION;
 import static subway.util.CompleteMessage.COMPLETE_STATION_DELETE;
 import static subway.util.CompleteMessage.COMPLETE_STATION_REGISTRATION;
+import static subway.util.CompleteMessage.DEFAULT_PRINT_MESSAGE;
 
 import java.util.List;
 import subway.domain.Line;
@@ -13,6 +14,7 @@ import subway.domain.Station;
 
 public class OutputView {
     private static final String NEW_LINE = System.lineSeparator();
+    private static final String DELIMITER = "---";
 
     public void printExceptionMessage(IllegalArgumentException exception) {
         System.out.println(exception.getMessage());
@@ -35,7 +37,7 @@ public class OutputView {
     }
 
     public void printStations(Station station) {
-        System.out.println("[INFO] " + station.getName());
+        System.out.println(DEFAULT_PRINT_MESSAGE.getMessage() + station.getName());
     }
 
     public void printLineMenu() {
@@ -47,7 +49,7 @@ public class OutputView {
     }
 
     public void printLines(Line line) {
-        System.out.println("[INFO] " + line.getName());
+        System.out.println(DEFAULT_PRINT_MESSAGE.getMessage() + line.getName());
     }
 
     public void printDeleteLine() {
@@ -73,8 +75,8 @@ public class OutputView {
     }
 
     private void printLineInfo(Line line) {
-        System.out.println("[INFO] " + line.getName());
-        System.out.println("[INFO] ---");
+        System.out.println(DEFAULT_PRINT_MESSAGE.getMessage() + line.getName());
+        System.out.println(DEFAULT_PRINT_MESSAGE.getMessage() + DELIMITER);
 
         printSections(line.getSections());
         System.out.print(NEW_LINE);
@@ -95,20 +97,20 @@ public class OutputView {
                 + "3. 구간 관리" + NEW_LINE
                 + "4. 지하철 노선도 출력" + NEW_LINE
                 + "Q. 종료" + NEW_LINE),
-        STATION_MENU("## 역 관리 화면" + NEW_LINE +
-                "1. 역 등록" + NEW_LINE +
-                "2. 역 삭제" + NEW_LINE +
-                "3. 역 조회" + NEW_LINE +
-                "B. 돌아가기"),
-        LINE_MENU("## 노선 관리 화면" + NEW_LINE +
-                "1. 노선 등록" + NEW_LINE +
-                "2. 노선 삭제" + NEW_LINE +
-                "3. 노선 조회" + NEW_LINE +
-                "B. 돌아가기"),
-        SECTION_MENU("## 구간 관리 화면" + NEW_LINE +
-                "1. 구간 등록" + NEW_LINE +
-                "2. 구간 삭제" + NEW_LINE +
-                "B. 돌아가기"),
+        STATION_MENU("## 역 관리 화면" + NEW_LINE
+                + "1. 역 등록" + NEW_LINE
+                + "2. 역 삭제" + NEW_LINE
+                + "3. 역 조회" + NEW_LINE
+                + "B. 돌아가기"),
+        LINE_MENU("## 노선 관리 화면" + NEW_LINE
+                + "1. 노선 등록" + NEW_LINE
+                + "2. 노선 삭제" + NEW_LINE
+                + "3. 노선 조회" + NEW_LINE
+                + "B. 돌아가기"),
+        SECTION_MENU("## 구간 관리 화면" + NEW_LINE
+                + "1. 구간 등록" + NEW_LINE
+                + "2. 구간 삭제" + NEW_LINE
+                + "B. 돌아가기"),
         SUBWAY_LIST_INTRO("## 지하철 노선도");
 
         private final String message;
@@ -116,5 +118,5 @@ public class OutputView {
         Message(String message) {
             this.message = message;
         }
-        }
+    }
 }
